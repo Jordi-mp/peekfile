a458e8b43a832124fe153979526ef5b2e5f44487
Jordi-mp <149062296+Jordi-mp@users.noreply.github.com>
2023-11-02 16:09:18 +0100

    peek.sh

--- peek.sh
+++ peek.sh
@@ -1,7 +1,7 @@
 #usr/bin/bash
 
 file="$1"
-lines="${2:-3}" 
+lines="${2:-3}"  #We use :- to provide a default value for a variable if the variable is unset or empty
 
 total_lines=$(wc -l < "$file")
 
